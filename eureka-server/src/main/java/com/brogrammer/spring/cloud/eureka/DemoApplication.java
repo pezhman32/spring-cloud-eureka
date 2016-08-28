8a6b476684ad6665e67da067a7956df0b2deafc3
Pezhman  Jahanmard <pezhman32@gmail.com>
2016-08-28 16:53:12 +0430

    init

---
+++ eureka-server/src/main/java/com/brogrammer/spring/cloud/eureka/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.brogrammer.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
